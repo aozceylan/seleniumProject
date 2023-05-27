7af3e4545e3d0fa7c33a4420b6f4def8c14bdbdb
Aysegul <aozceylan26@gmail.com>
2023-05-27 22:29:38 +0200

    version 27.05

--- src/test/java/tecproEducation/day01_ilkTest_DriverMethods/C01_ilkTest.java
+++ src/test/java/tecproEducation/day01_ilkTest_DriverMethods/C01_ilkTest.java
@@ -1,9 +1,9 @@
-package techproed.day01_ilkTest_DriverMethods;
+package tecproEducation.day01_ilkTest_DriverMethods;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Ders01 {
+public class C01_ilkTest {
     public static void main(String[] args) {
         System.setProperty("chromeDriver","src/resources/drivers/chromedriver.exe") ;
         //Java uygulamalarinda system ozelliklerini ayarlamak icin setProperties() methodu ile kullaniriz
--- src/test/java/tecproEducation/day01_ilkTest_DriverMethods/C02_DriverMethods.java
+++ src/test/java/tecproEducation/day01_ilkTest_DriverMethods/C02_DriverMethods.java
@@ -1,4 +1,4 @@
-package techproed.day01_ilkTest_DriverMethods;
+package tecproEducation.day01_ilkTest_DriverMethods;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
